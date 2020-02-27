adf11c65612a452983c41e4d0d5c7dbddd369524
magicEmperor <631960470@qq.com>
2020-02-27 14:08:34 +0800

    实现翻页抓取

--- QuotesSpider/QuotesSpider/items.py
+++ QuotesSpider/QuotesSpider/items.py
@@ -11,4 +11,6 @@ import scrapy
 class QuotesspiderItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
-    pass
+    text = scrapy.Field()
+    author = scrapy.Field()
+    tags = scrapy.Field()
--- QuotesSpider/QuotesSpider/spiders/QSpider.py
+++ QuotesSpider/QuotesSpider/spiders/QSpider.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 import scrapy
 
+from QuotesSpider.items import QuotesspiderItem
+
 
 class QspiderSpider(scrapy.Spider):
     name = 'QSpider'
@@ -8,4 +10,16 @@ class QspiderSpider(scrapy.Spider):
     start_urls = ['http://quotes.toscrape.com//']
 
     def parse(self, response):
-        pass
+        item = QuotesspiderItem()
+        quotes = response.css('.quote')
+        for quote in quotes:
+            text = quote.css('.text::text').extract_first()
+            author = quote.css('.author::text').extract_first()
+            tags = quote.css('.tags .tag::text').extract()
+            item['text'] = text
+            item['author'] = author
+            item['tags'] = tags
+            yield item
+        next = response.css('.pager .next a::attr(href)').extract_first()
+        url = response.urljoin(next)
+        yield scrapy.Request(url=url,callback=self.parse)

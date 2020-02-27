72922d26498cbb9cb4b11d99ae672697dc3340c7
magicEmperor <631960470@qq.com>
2020-02-27 10:42:58 +0800

    修改路由信息

--- QuotesSpider/QuotesSpider/spiders/QSpider.py
+++ QuotesSpider/QuotesSpider/spiders/QSpider.py
@@ -4,8 +4,8 @@ import scrapy
 
 class QspiderSpider(scrapy.Spider):
     name = 'QSpider'
-    allowed_domains = ['http://quotes.toscrape.com/']
-    start_urls = ['http://http://quotes.toscrape.com//']
+    allowed_domains = ["quotes.toscrape.com"]
+    start_urls = ['http://quotes.toscrape.com//']
 
     def parse(self, response):
         pass

c8d273fd2fdf124b4bbcc143c0db96083e174084
magicEmperor <631960470@qq.com>
2020-03-02 20:05:05 +0800

    两个接口

--- UserInfo/UserInfo/spiders/UserSpider.py
+++ UserInfo/UserInfo/spiders/UserSpider.py
@@ -6,6 +6,9 @@ class UserspiderSpider(scrapy.Spider):
     name = 'UserSpider'
     allowed_domains = ['www.zhihu.com']
     start_urls = ['http://www.zhihu.com/']
-
+    def start_requests(self):
+        # url = 'https://www.zhihu.com/people/lengyue233'
+        url = 'https://www.zhihu.com/api/v4/members/Germey/followees?include=data%5B*%5D.answer_count%2Carticles_count%2Cgender%2Cf'
+        yield scrapy.Request(url=url,callback=self.parse)
     def parse(self, response):
-        pass
+        print(response.text)
